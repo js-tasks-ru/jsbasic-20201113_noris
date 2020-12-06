f9ad0d95f20b590adb59804d820db205b8aa52f2
RadioMan116 <noris75564@gmail.com>
2020-12-06 21:36:06 +0300

    add 4-th module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -5,4 +5,17 @@
  */
 function makeFriendsList(friends) {
   // ваш код...
+  let ul = document.createElement("ul");
+
+  friends.map( item => {
+
+	let li = document.createElement("li");
+
+	ul.appendChild(li);
+	li.innerHTML = `${item.firstName} ${item.lastName}`;
+  })
+
+  document.body.appendChild(ul);
+
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -3,4 +3,33 @@
  * @param {Element} table
  */
 function highlight(table) {
+
+	let rows = table.querySelector("tbody").children;
+
+	for(let child of rows) {
+
+		let status = child.cells[3]
+		let gender = child.cells[2]
+		let age = child.cells[1]
+
+		if(status.getAttribute("data-available") === "true") {
+			child.classList.add("available")
+		} else if(status.getAttribute("data-available") === "false") {
+			child.classList.add("unavailable")
+		}
+
+		if(!status.hasAttribute("data-available")) {
+			child.setAttribute("hidden", "")
+		}
+
+		if(gender.innerHTML == "m"){
+			child.classList.add("male")
+		} else if (gender.innerHTML == "f") {
+			child.classList.add("female")
+		}
+
+		if(age.innerHTML < 18) {
+			child.style="text-decoration: line-through";
+		}
+	}
 }
