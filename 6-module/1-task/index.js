00153c1b372ad0710e19bec03ee1ce0425bc29a0
RadioMan116 <noris75564@gmail.com>
2020-12-21 11:28:49 +0300

    80%

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -5,11 +5,11 @@
  * Пример одного элемента, описывающего строку таблицы
  *
  *      {
-     *          name: '',
-     *          age: 25,
-     *          salary: '1000',
-     *          city: 'Petrozavodsk'
-     *   },
+	 *          name: '',
+	 *          age: 25,
+	 *          salary: '1000',
+	 *          city: 'Petrozavodsk'
+	 *   },
  *
  * @constructor
  */
@@ -29,6 +29,50 @@
  * @constructor
  */
 export default class UserTable {
-  constructor(rows) {
-  }
+	constructor(rows) {
+		this.data = rows;
+		this.elem = this.render();
+	}
+
+	render() {
+		this.table = document.createElement("table");
+		let template = `
+		<thead>
+				<tr>
+					<th>Имя</th>
+					<th>Возраст</th>
+					<th>Зарплата</th>
+					<th>Город</th>
+					<th></th>
+				</tr>
+		</thead>
+		<tbody></tbody>`;
+
+		this.table.insertAdjacentHTML("beforeend", template);
+		let tbody = this.table.querySelector("tbody");
+
+		for (let prop of this.data) {
+
+			let tmp = `
+				<tr>
+					<td>${prop.name}</td>
+					<td>${prop.age}</td>
+					<td>${prop.salary}</td>
+					<td>${prop.city}</td>
+					<td>
+						<button>X</button>
+					</td>
+				</tr>`;
+
+			tbody.insertAdjacentHTML("beforeend", tmp);
+		}
+
+		this.table.addEventListener("click", e => {
+			if (e.target.closest("button")) {
+				e.target.closest('tr').remove();
+			};
+		});
+
+		return this.table;
+	}
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,96 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
-  constructor(slides) {
-    this.slides = slides;
-  }
+	constructor(slides) {
+		this.slides = slides;
+		this.elem = this.render();
+	}
+	render() {
+		//Создаем родительский элемент
+		// this.elem = document.createElement("div");
+		let carousel = document.createElement("div");
+		carousel.classList = "carousel";
+		let templateButton = `
+			<div class="carousel__arrow carousel__arrow_right">
+				<img src="/assets/images/icons/angle-icon.svg" alt="icon">
+			</div>
+			<div class="carousel__arrow carousel__arrow_left">
+				<img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+			</div>
+			<div class="carousel__inner"></div>`;
+
+		carousel.insertAdjacentHTML("beforeend", templateButton);
+		let carouselInner = carousel.querySelector(".carousel__inner");
+
+		//Создаем слайды
+		for (let slide of this.slides) {
+			let templateSlides = `
+				<div class="carousel__slide" data-id="${slide.id}">
+					<img src="/assets/images/carousel/${slide.image}" class="carousel__img" alt="slide">
+					<div class="carousel__caption">
+						<span class="carousel__price">€${slide.price.toFixed(2)}</span>
+						<div class="carousel__title">${slide.name}</div>
+						<button type="button" class="carousel__button">
+							<img src="/assets/images/icons/plus-icon.svg" alt="icon">
+						</button>
+					</div>
+				</div>`;
+
+			carouselInner.insertAdjacentHTML("beforeend", templateSlides)
+		}
+
+		//Генерация событий
+		let button = carousel.querySelector(".carousel__button");
+		let event = new CustomEvent("product-add", {
+			detail: button.parentNode.parentNode.dataset.id,
+			bubbles: true,
+		})
+		button.addEventListener("click", () => {
+			carousel.dispatchEvent(event);
+		})
+
+		// setTimeout(() => {
+		//Предыдущий код слайдера
+
+		let buttonLeft = carousel.querySelector(".carousel__arrow_left");
+		let buttonRight = carousel.querySelector(".carousel__arrow_right");
+		let sliderInner = carousel.querySelector(".carousel__inner");
+		let sliderWidth
+
+		let sum = 0;
+
+		document.addEventListener("DOMContentLoaded", () => {
+
+			sliderWidth = document.querySelector(".carousel__slide").offsetWidth;
+			console.log(sliderWidth)
+
+		})
+
+		buttonLeft.style.display = "none";
+		buttonRight.onclick = () => {
+
+			buttonLeft.style.display = "flex";
+			sum -= sliderWidth;
+			sliderInner.style.transform = `translateX(${sum}px)`;
+
+			if (sum == -sliderWidth * 3) {
+				buttonRight.style.display = "none";
+			}
+
+		};
+
+		buttonLeft.onclick = () => {
+
+			buttonRight.style.display = "flex";
+			sum += sliderWidth
+			sliderInner.style.transform = `translateX(${sum}px)`;
+
+			if (sum == 0) {
+				buttonLeft.style.display = "none";
+			}
+		};
+		// }, 0)
+
+		return carousel;
+	}
 }
