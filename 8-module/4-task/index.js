72c1e74832021002b2f23cf19221162b3f773a0a
RadioMan116 <noris75564@gmail.com>
2021-01-06 21:36:06 +0300

    8th module

--- 8-module/4-task/index.js
+++ 8-module/4-task/index.js
@@ -104,66 +104,82 @@ export default class Cart {
 	}
 
 	renderProduct(product, count) {
-		// console.log("product: ", product)
-		//product.forEach(item => {
-		// console.log(count)
 		return createElement(`
-				<div class="cart-product" data-product-id="${product.id}">
-					<div class="cart-product__img">
-						<img src="/assets/images/products/${product.image}" alt="product">
+			<div class="cart-product" data-product-id="${product.id}">
+				<div class="cart-product__img">
+					<img src="/assets/images/products/${product.image}" alt="product">
+				</div>
+				<div class="cart-product__info">
+					<div class="cart-product__title">${product.name}</div>
+					<div class="cart-product__price-wrap">
+					<div class="cart-counter">
+						<button type="button" class="cart-counter__button cart-counter__button_minus">
+						<img src="/assets/images/icons/square-minus-icon.svg" alt="minus">
+						</button>
+						<span class="cart-counter__count">${count}</span>
+						<button type="button" class="cart-counter__button cart-counter__button_plus">
+						<img src="/assets/images/icons/square-plus-icon.svg" alt="plus">
+						</button>
 					</div>
-					<div class="cart-product__info">
-						<div class="cart-product__title">${product.name}</div>
-						<div class="cart-product__price-wrap">
-						<div class="cart-counter">
-							<button type="button" class="cart-counter__button cart-counter__button_minus">
-							<img src="/assets/images/icons/square-minus-icon.svg" alt="minus">
-							</button>
-							<span class="cart-counter__count">${count}</span>
-							<button type="button" class="cart-counter__button cart-counter__button_plus">
-							<img src="/assets/images/icons/square-plus-icon.svg" alt="plus">
-							</button>
-						</div>
-						<div class="cart-product__price">€${product.price.toFixed(2)}</div>
-						</div>
+					<div class="cart-product__price">€${product.price.toFixed(2)}</div>
 					</div>
-				</div>`);
-		//})
+				</div>
+			</div>`);
 	}
 
 	renderOrderForm() {
 		return createElement(`<form class="cart-form">
-      <h5 class="cart-form__title">Delivery</h5>
-      <div class="cart-form__group cart-form__group_row">
-        <input name="name" type="text" class="cart-form__input" placeholder="Name" required value="Santa Claus">
-        <input name="email" type="email" class="cart-form__input" placeholder="Email" required value="john@gmail.com">
-        <input name="tel" type="tel" class="cart-form__input" placeholder="Phone" required value="+1234567">
-      </div>
-      <div class="cart-form__group">
-        <input name="address" type="text" class="cart-form__input" placeholder="Address" required value="North, Lapland, Snow Home">
-      </div>
-      <div class="cart-buttons">
-        <div class="cart-buttons__buttons btn-group">
-          <div class="cart-buttons__info">
-            <span class="cart-buttons__info-text">total</span>
-            <span class="cart-buttons__info-price">€${this.getTotalPrice().toFixed(
-			2
-		)}</span>
-          </div>
-          <button type="submit" class="cart-buttons__button btn-group__button button">order</button>
-        </div>
-      </div>
-    </form>`);
+				<h5 class="cart-form__title">Delivery</h5>
+				<div class="cart-form__group cart-form__group_row">
+					<input name="name" type="text" class="cart-form__input" placeholder="Name" required value="Santa Claus">
+					<input name="email" type="email" class="cart-form__input" placeholder="Email" required value="john@gmail.com">
+					<input name="tel" type="tel" class="cart-form__input" placeholder="Phone" required value="+1234567">
+				</div>
+				<div class="cart-form__group">
+					<input name="address" type="text" class="cart-form__input" placeholder="Address" required value="North, Lapland, Snow Home">
+				</div>
+				<div class="cart-buttons">
+					<div class="cart-buttons__buttons btn-group">
+					<div class="cart-buttons__info">
+						<span class="cart-buttons__info-text">total</span>
+						<span class="cart-buttons__info-price">€${this.getTotalPrice().toFixed(2)}</span>
+					</div>
+					<button type="submit" class="cart-buttons__button btn-group__button button">order</button>
+					</div>
+				</div>
+			</form>`);
 	}
 
 	renderModal() {
 		// ...ваш код
-		console.log(111)
-		let modal = new Modal();
-		modal.setTitle("Your order")
+		// let [product] = this.cartItems;
+		// console.log("product", product)
+		// console.log("this.cartItems", this.cartItems)
 
-		modal.setBody(this.renderProduct(this.cartItems.forEach(product => product)))
-		// modal.setBody()
+		let a = this.cartItems.forEach(item => {
+			return item.product;
+		})
+
+		// console.log("a", a)
+		// console.log(this.renderProduct(this.cartItems[0].product, this.cartItems[0].count))
+
+		let template = createElement(`<div>Hello</div>`);
+		for (let prop of this.cartItems) {
+			template.insertAdjacentElement("beforeend", this.renderProduct(prop.product, prop.count))
+		}
+
+		console.log(template)
+
+		let modal = new Modal();
+		modal.setTitle("Your order");
+
+
+		modal.setBody(template)
+
+		// modal.setBody(this.cartItems.forEach(item => {
+		// 	this.renderProduct(item.product, item.product.count);
+		// }));
+
 		modal.open()
 	}
 
